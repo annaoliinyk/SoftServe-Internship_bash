f11fc5e398e32a15279b0883d16dc636cf10b981
Anna Oliinyk <anoliinyk@softserveinc.com>
2022-08-03 15:48:34 +0200

    2nd task for bash added

---
+++ read_many_arguments.sh
@@ -0,0 +1,8 @@
+# write a bash script to show arguments (2 or 3) obtained from command line
+echo "Enter your arguments (up to 3, separated by new line):"
+read a
+read b
+read c
+
+echo "You entered: $a, $b, $c"
+

b7e80961f438e693adb0f90ecd8dcad44c01b39f
Anna Oliinyk <anoliinyk@softserveinc.com>
2022-08-03 15:47:23 +0200

    3rd task for bash added

---
+++ calculate_sum.sh
@@ -0,0 +1,7 @@
+# write a bash script to calculate the sum of two numbers which will be entered by user                          
+echo "Please enter 2 numbers, each one from new line:"
+read a
+read b
+sum=$((a+b))
+echo "Sum is $sum"
+
